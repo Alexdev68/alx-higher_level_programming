74c55c31092c86a8368e38d2532019ae4a24a8de
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-09 08:20:00 -0700

    Bug fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,7 @@ for i in range(0, 9):
     for x in range(1, 10):
         if i == 8 and x == 9:
             continue
+        if i == x:
+            continue
         print("{}{}, ".format(i, x), end='')
 print("{}{}".format(8, 9))
