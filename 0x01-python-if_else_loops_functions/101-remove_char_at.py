a231117ef63b00c8316b57cbc592e76280da39e9
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-09 15:56:57 -0700

    New file

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def remove_char_at(str, n):
+    mod_str = ''
+    for i in range(len(str)):
+        if i != n:
+            mod_str += str[i]
+    print("{}".format(mod_str))
