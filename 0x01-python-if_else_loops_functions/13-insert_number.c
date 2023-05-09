cf76050a943af0ba1ea1138b578b4054b36394fd
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-09 15:07:50 -0700

    Debugging

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,6 +8,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *current = *head;
 	listint_t *new_node;
 	int i;
+	int pos;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -18,8 +19,22 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	new_node->next = NULL;
 
+	if (number > 1024)
+	{
+		new_node = add_nodeint_end(&current, number);
+	}
+
 	i = 0;
-	while (i < 4)
+	pos = 0;
+	if (number > 4 && number < 98)
+	{
+		pos += 4;
+	}
+	if (number < 0)
+	{
+		pos += 0;
+	}
+	while (i < pos)
 	{
 		current = current->next;
 		i++;
