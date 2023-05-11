0f42e6c0dfb65423f4e2e6a9a9451907b5f42786
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-11 09:19:15 -0700

    New file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv
+no_args = len(argv)
+
+if len(argv) - 1 > 1:
+    print("{} arguments:".format(no_args - 1))
+if len(argv) - 1 == 0:
+    print("{} arguments.".format(no_args - 1))
+if len(argv) - 1 == 1:
+    print("{} argument:".format(no_args - 1))
+
+for i in range(1, no_args):
+    print("{}: {}".format(i, argv[i]))
