5fa355980605903700b90b21e402af0c602d459a
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-11 15:43:11 -0700

    Debugging

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    add, sub = magic_calculation_102.add, magic_calculation_102.subi
-    c = 0
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i in range(0, 90):
+        for i in range(4, 6):
             c = add(c, i)
         return c
     else:
