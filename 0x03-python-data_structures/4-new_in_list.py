fba24a2582721168b805e042f0783b1ffd9fd1f6
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-12 06:26:29 -0700

    New file

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    cpy_list = my_list[:]
+    
+    if idx < 0:
+        return cpy_list
+    if idx > len(my_list) - 1:
+        return cpy_list
+
+    cpy_list[idx] = element
+    return cpy_list
