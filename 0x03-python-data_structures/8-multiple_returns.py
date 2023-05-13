f594dd57391fa3c6811854b5d5ba81db78e682fa
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-13 14:22:40 -0700

    Debugging

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     a = len(sentence)
-    b = sentence[0]
 
     if a == 0:
-        sentence[0] == None
+        b = None
+    else:
+        b = sentence[0]
 
     tuple_weird = (a, b)
     return tuple_weird
