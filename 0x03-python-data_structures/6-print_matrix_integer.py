bcdaea48a42f20493f113bdd18694982349f3f9e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-13 10:37:05 -0700

    Code revamped

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+
     for i in matrix:
-        print(' '.format().join(map(str, i)))
+        for j, el in enumerate(i):
+            if j == len(i) - 1:
+                print("{:d}".format(el), end='')
+            else:
+                print("{:d} ".format(el), end='')
+        print()
