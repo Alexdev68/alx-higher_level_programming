e3143bd9c58f7b497e47b2a260d1707a6a98bede
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-13 14:14:01 -0700

    Fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -12,7 +12,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         elif len(tuple_b) == 0:
             tuple_b = (0, 0)
 
-    if  len(tuple_a) >= 2 or len(tuple_b) >= 2:
+    if len(tuple_a) >= 2 or len(tuple_b) >= 2:
         hyb_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
         return hyb_tuple
 
