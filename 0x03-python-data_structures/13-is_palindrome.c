8806d5421a987d1209eb4a9809fe871fed128284
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-14 05:21:34 -0700

    New files

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - This function checks if a linked list is a palindrome
+ * @head: This is a pointer to the head of the linked list
+ * Return: This returns 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head;
+	char *s_str;
+	int i = 0, j = 0, len = 0, k;
+
+	if (current == NULL)
+		return (1);
+
+	while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+	s_str = malloc(sizeof(char) * (len + 1));
+	current = *head;
+	while (current != NULL)
+	{
+		s_str[i] = current->n;
+		current = current->next;
+		i++;
+	}
+	s_str[i] = '\0';
+
+	char r_str[len + 1];
+
+	for (k = len - 1; k >= 0; k--)
+	{
+		r_str[j] = s_str[k];
+		j++;
+	}
+	r_str[j] = '\0';
+	i = 0;
+	j = 0;
+	while (i < len && j < len)
+	{
+		if (s_str[i] != r_str[j])
+			break;
+		i++;
+		j++;
+	}
+	if (s_str[i] == '\0' && r_str[j] == '\0')
+		return (1);
+	return (0);
+}
