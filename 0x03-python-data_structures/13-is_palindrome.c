3a162da672bb7fe4706b843f417256228887cb01
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-14 09:28:57 -0700

    Debugging

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -18,7 +18,7 @@ int is_palindrome(listint_t **head)
 	for (i = 0; current != NULL; current = current->next, i++)
 		len++;
 	s_str = malloc(sizeof(int) * (len + 1));
-	i = 0
+	i = 0;
 	for (current = *head; current != NULL; current = current->next, i++)
 		s_str[i] = current->n;
 	s_str[i] = '\0';
