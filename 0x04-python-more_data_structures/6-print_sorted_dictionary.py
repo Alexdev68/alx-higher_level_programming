dcdf0993f1a09e17c7ffb728517cf436f0ba9828
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-17 08:58:09 -0700

    Alteration

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    my_dict = sorted(a_dictionary.items())
+    sort = sorted(a_dictionary.items())
+    my_dict = dict(sort)
 
     for i in my_dict:
-        print("{}: {}".format(i[0], i[1]))
+        print(i, ':', my_dict[i])
