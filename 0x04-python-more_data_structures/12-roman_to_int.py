58ba7cca90e6098bf96b5f2451817de502206362
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-17 12:15:12 -0700

    New file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    i = 0
+    integer = 0
+    st = roman_string
+    
+    while i < len(st):
+        ch1 = numerals[st[i]]
+        if i + 1 < len(st):
+            ch2 = numerals[st[i + 1]]
+            if (ch1 >= ch2):
+                integer += ch1
+                i += 1
+            else:
+                integer -= ch1
+                i += 1
+        else:
+            integer += ch1
+            i += 1
+
+    return integer
