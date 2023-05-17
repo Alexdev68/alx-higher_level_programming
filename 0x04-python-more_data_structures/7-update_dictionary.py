150bf06b8804a08c73c15b99d3f9608cc32a5323
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-17 09:48:59 -0700

    Fixed

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if not a_dictionary:
-        return
+    a_dictionary.update({key: value})
 
-    for i in a_dictionary:
-        a_dictionary.update({key: value})
-
-        return a_dictionary
+    return a_dictionary
