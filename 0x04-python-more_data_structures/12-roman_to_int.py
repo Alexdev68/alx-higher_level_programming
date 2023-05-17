7db81ac51985c75efbeed4f692c0c84314eb145b
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-17 12:37:14 -0700

    Fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+numerals = {
+    'I': 1,
+    'V': 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+}
+
+
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) != str:
         return 0
-
-    numerals = {
-            'I': 1, 
-            'V': 5, 
-            'X': 10, 
-            'L': 50, 
-            'C': 100, 
-            'D': 500, 
-            'M': 1000}
 
     i = 0
     integer = 0
