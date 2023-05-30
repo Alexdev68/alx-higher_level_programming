f40910096105185a604120455a4938576389c1bd
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-30 06:41:31 -0700

    New files

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Docstring for this module.
+
+This module house's an empty class ``Square`` that defines a square
+"""
+class Square:
+    """This is an empty class ``Square`` that defines a square"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+# 0x06. Python - Classes and Objects
+This project contains an introduction and tasks under python classes and objects
