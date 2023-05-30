49526c69136943a606c68df364c0d9207bd660a1
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-30 07:20:30 -0700

    New file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module house's a class ``Square`` that defines a square."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, _size):
+        """__init__ method.
+
+        Arg:
+            _size(no type/value verification): Private instance attribute.
+        """
+        self._Square__size = _size
