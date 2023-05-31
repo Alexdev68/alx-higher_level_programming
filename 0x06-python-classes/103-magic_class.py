24bea1c65da5ea18e0f79489fa2efe882234825f
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-30 18:31:30 -0700

    Debugging

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -22,7 +22,7 @@ class MagicClass:
         self._MagicClass__radius = 0
 
         if type(radius) is not int:
-            type(radius) is not float:
+            if type(radius) is not float:
                 raise TypeError('radius must be a number')
         self._MagicClass__radius = radius
         return None
