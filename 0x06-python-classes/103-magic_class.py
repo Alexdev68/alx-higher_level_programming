039ac8b965c3873bd38398d088817037986090b3
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-05-30 18:25:29 -0700

    Debugging

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
+"""This module houses a class ``MagicClass`` and imports math for pi."""
 import math
 
 
 class MagicClass:
+    """This is a magic class that perfoorms a magic calcultion."""
 
     def __init__(self, radius):
+        """This method assigns zero to self._MagicClass__radius the radius
+
+        Arg:
+            radius(int or float): This is a value that can be either float or
+             int
+
+        Raises:
+            TypeError: If the type of radius is not an int of float
+
+        Returns:
+            None
+        """
         self._MagicClass__radius = 0
 
         if type(radius) is not int or type(radius) is not float:
@@ -13,7 +27,17 @@ class MagicClass:
         return None
 
     def area(self):
+        """area method
+
+        Returns:
+            int: (self._MagicClass__radius ** 2) * math.pi)
+        """
         return (self._MagicClass__radius ** 2) * math.pi
 
     def circumference(self):
+        """circumference method
+
+        Returns:
+            int: (2 * math.pi) * self._MagicClass__radius
+        """
         return (2 * math.pi) * self._MagicClass__radius
