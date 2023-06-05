e10c35d47e3795029a002e0267a07e68be05416a
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-05 08:00:36 -0700

    Fixing issues

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,9 +9,24 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """This instance method initializes self._Rectangle__width and \
                 self._Rectangle__height.
+
+            Raises:
+                TypeError: If self._Rectangle__width is not of type int. \
+                        If self._Rectangle__height is not of type int.
+                ValueError: If self._Rectangle__width is less than zero. \
+                        If self._Rectangle__height is less than zero.
         """
         self._Rectangle__width = width
         self._Rectangle__height = height
+
+        if not isinstance(self._Rectangle__width, int):
+            raise TypeError('width must be an integer')
+        if self._Rectangle__width < 0:
+            raise ValueError('width must be >= 0')
+        if not isinstance(self._Rectangle__height, int):
+            raise TypeError('height must be an integer')
+        if self._Rectangle__height < 0:
+            raise ValueError('height must be >= 0')
 
     @property
     def width(self):
