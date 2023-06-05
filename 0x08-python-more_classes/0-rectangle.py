f9dc925cb9bcbccc9aa7013b6f8ececb189018ed
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-05 06:34:41 -0700

    New files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module houses a function that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """This is an empty class.
+    """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+# 0x08. Python - More Classes and Objects
+This project takes us deeper into python's classes and objects
