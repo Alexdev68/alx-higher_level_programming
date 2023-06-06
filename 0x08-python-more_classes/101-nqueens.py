767330203a0adb0a5fbe789e62b9f966ff049a69
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-06 03:40:57 -0700

    Fixed some test case

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
 
-N = int(argv[1])
-
 if len(argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-if not isinstance(N, int):
+if not isinstance(argv[1], int):
     print("N must be a number")
     exit(1)
+
+N = int(argv[1])
 
 if N < 4:
     print("N must be at least 4")
