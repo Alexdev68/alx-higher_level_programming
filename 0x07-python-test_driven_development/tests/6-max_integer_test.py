97afd75f3b7d1054144c3ad413117f09b38f5e3e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-08 13:38:06 -0700

    New test cases added

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -13,6 +13,8 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-34, -57, -123, -110]), -34)
         self.assertEqual(max_integer([12.7, 12.6, 12.9, 12.2]), 12.9)
         self.assertEqual(max_integer([-21, 4.2, 6.3, -55]), 6.3)
+        self.assertEqual(max_integer([25]), 25)
+        self.assertEqual(max_integer([56, 9, 39, 222]), 222)
 
     def test_return_none(self):
         self.list = []
