377d00581217143c3a65e267ce2f744b1bb9ceb7
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-08 13:58:41 -0700

    Some fixes were made

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """This module houses a function tha indents a text.
 """
+
+
 def text_indentation(text):
     """This function indents a text due to the appearance of some characters.
     """
+    if len(text) == 0:
+        raise TypeError("text must not be empty")
+
     if not isinstance(text, str):
         raise TypeError('text must be a string')
 
