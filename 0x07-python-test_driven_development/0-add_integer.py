881f80d8cbfcd2d19a63de7f7e2e63e2c31711a9
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-08 17:28:00 -0700

    Added some tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,6 +14,12 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
+    if a > 1000000 or b > 1000000:
+        raise ValueError('Float overflow')
+    if not a > 0 and not a < 0 and type(a) is float:
+        raise ValueError('cannot convert float NaN to integer')
+    if not b > 0 and not b < 0 and type(b) is float:
+        raise ValueError('cannot convert float NaN to integer')
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
