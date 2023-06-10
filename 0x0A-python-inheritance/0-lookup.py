8f3018f1076178297057085881e6f61282eaaca6
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-10 12:46:41 -0700

    New file

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a function that lists all the attributes and methods \
+        of an object.
+"""
+def lookup(obj):
+    """This function returns a list of all the attributes and method of an \
+            object.
+    """
+    return dir(obj);
