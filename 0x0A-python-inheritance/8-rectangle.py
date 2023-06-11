191fb4788e8092578c895fb99d4ed265f4abfc86
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 08:25:25 -0700

    Added documentation

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
+"""This module import a class from 7-base_goemetry and the class defined here \
+        inherits it.
+"""
 base_geometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(base_geometry):
+    """This class contains a method that validates it's args by inheriting \
+            from the parent class.
+    """
     def __init__(self, width, height):
+        """This method inherits integer_validator to validate width and height.
+        """
         self.__width = width
         self.__height = height
 
