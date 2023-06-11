d4eda9ae8cf6904a2c01da260c7c236d9f6ce39f
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 11:55:37 -0700

    Reverted back

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -17,7 +17,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         self.__name = name
         self.__value = value
-        if not type(self.__value) is  (int):
+        if not type(self.__value) is (int):
             raise TypeError(f'{self.__name} must be an integer')
         if self.__value <= 0:
             raise ValueError(f'{self.__name} must be greater than 0')
