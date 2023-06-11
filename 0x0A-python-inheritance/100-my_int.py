2a76e12a3b6b871ec7c4b08392fd22aade985c5e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 14:04:53 -0700

    Added documentation

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
+"""This module contains a class ``MyInt``.
+"""
+
+
 class MyInt(int):
+    """This class is a rebel and it changes equal to, to not equal to and \
+            vice versa.
+    """
 
     def __eq__(self, other):
+        """This method changes equal to, to not equal to.
+        """
         return self.real != other
 
     def __ne__(self, other):
+        """This method changes not equal to, to equal to.
+        """
         return self.real == other
