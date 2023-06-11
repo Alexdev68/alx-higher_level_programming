603a0cac72b97e506603cbc456c9dbe02ae050d6
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 11:16:56 -0700

    Removed init method

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,12 +6,6 @@
 class MyList(list):
     """This class print a sorted list in ascending order.
     """
-
-    def __init__(self):
-        """This is the initailization method.
-        """
-        super().__init__()
-
     def print_sorted(self):
         """This method prints the sorted list.
         """
