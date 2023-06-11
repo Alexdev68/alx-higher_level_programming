c0e75d33ff2fff0d1f6ae32de159b725ea1970ad
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 08:42:06 -0700

    New file

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module import a class from 7-base_goemetry and the class defined here \
+        inherits it.
+"""
+base_geometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(base_geometry):
+    """This class contains a method that validates it's args by inheriting \
+            from the parent class.
+    """
+    def __init__(self, width, height):
+        """This method inherits integer_validator to validate width and height.
+        """
+        self.__width = width
+        self.__height = height
+
+        base_geometry().integer_validator("width", self.__width)
+        base_geometry().integer_validator("height", self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def my_print(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __str__(self):
+        return self.my_print()
