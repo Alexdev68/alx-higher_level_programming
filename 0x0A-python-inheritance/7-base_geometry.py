64b17c4deb27b1ec41807f50f1b70a61caff98e5
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 07:13:46 -0700

    New files

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains a class ``BaseGeometry``.
+"""
+
+
+class BaseGeometry:
+    """This class contains a or some methods that serve different purposes.
+    """
+    def area(self):
+        """This method doesn't calculate area.
+
+        Raises:
+            Exception: if area() is called.
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
