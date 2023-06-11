853d10f4d377848b526aebff11c95ff2eb0ae349
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 12:03:20 -0700

    New file

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module import a class from 9-rectangle and the class defined here \
+        inherits it.
+"""
+rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(rectangle):
+    """This class contains a method that validates it's args by inheriting \
+            from the parent class.
+    """
+    def __init__(self, size):
+        """This method inherits integer_validator to validate size.
+
+        Arg:
+            size(int): This is the size of the square
+        """
+
+        super().__init__(size, size)
+
+        self.__size = size
+        self.integer_validator("size", self.__size)
+
+    def area(self):
+        """This method returns the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
