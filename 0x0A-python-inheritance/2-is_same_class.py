8fc3df9e4d30626f01077e3bff8d40916bed3ad8
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 00:10:07 -0700

    New file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a function that returns bool based on some conditions.
+"""
+
+
+def is_same_class(obj, a_class):
+    """This function returns true if obj is an instance of class a_class.
+    """
+    if a_class == object:
+        return False
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
