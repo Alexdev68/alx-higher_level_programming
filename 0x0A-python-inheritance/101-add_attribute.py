c0258b56e42e0772886bd5e625b95ab436013a77
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-11 15:08:26 -0700

    New file

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a function that add a new attribute to a class.
+"""
+def add_attribute(sh_cls, name, value):
+    """This function checks if sh_cls is of any normal type then it raises an \
+            exception. 
+        If it's type is not a normal type from that tuple then it adds the new\
+                 attribute.
+    """
+    if type(sh_cls) in (int, str, None, tuple, list, bool, float, dict, type):
+        raise TypeError('can\'t add new attribute')
+
+    setattr(sh_cls, name, value)
