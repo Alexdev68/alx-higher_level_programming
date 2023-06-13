9855ac857b9b349f5d02d3f87eb16b497cc99db7
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-13 11:10:08 -0700

    New file

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a function that returns a dictionary description of \
+        a class.
+"""
+
+
+def class_to_json(obj):
+    """This is the function that returns a dictionary description of a class.
+    """
+    return vars(obj_2)
