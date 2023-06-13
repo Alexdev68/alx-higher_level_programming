f5785b9fb11d2a2d004c54a5c0321afcd4406f67
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-13 05:33:39 -0700

    New file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module contains a function that reads from a file.
+"""
+def read_file(filename=""):
+    """This function reads from a file and prints out it's content.
+    """
+    with open(filename, 'r', encoding="UTF8") as d:
+        read_info = d.read()
+
+    print(read_info, end='')
