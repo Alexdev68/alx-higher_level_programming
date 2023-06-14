253a64d3f9ac17ce9639d0e1f8502052e111159a
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-14 14:17:03 -0700

    Modified

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,6 +9,11 @@ try:
     all_stats = ("200", "301", "400", "401", "403", "404", "405", "500")
     my_dict = {}
     for line in stdin:
+        if count == 10:
+            print(f"File size: {tot_size}")
+            for key in sorted(my_dict.keys()):
+                print(f"{key}: {my_dict[key]}")
+            count = 0
         count += 1
         lst_line = line.split()
         tot_size += int(lst_line[8])
@@ -17,14 +22,9 @@ try:
                 my_dict[lst_line[7]] = my_dict.get(lst_line[7], 0) + 1
         except IndexError:
             pass
-        if count == 10:
-            print(f"File size: {tot_size}")
-            for key in sorted(my_dict.keys()):
-                print(f"{key}: {my_dict[key]}")
-            count = 0
-        print(f"File size: {tot_size}")
-        for key in sorted(my_dict.keys()):
-            print(f"{key}: {my_dict[key]}")
+    print(f"File size: {tot_size}")
+    for key in sorted(my_dict.keys()):
+        print(f"{key}: {my_dict[key]}")
 except KeyboardInterrupt:
     print(f"File size: {tot_size}")
     for key in sorted(my_dict.keys()):
