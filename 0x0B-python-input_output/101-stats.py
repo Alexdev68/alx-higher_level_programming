d744ca7e542c2f1c97880f7cadd8a0e508283f48
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-06-14 12:40:42 -0700

    Modifying

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,6 +9,7 @@ def handle_interrupt(signal, frame):
     print(f"File size: {tot_size}")
     for key in sorted(my_dict.keys()):
         print(f"{key}: {my_dict[key]}")
+    raise KeyboardInterrupt
     sys.exit(0)
 
 
@@ -16,12 +17,14 @@ signal.signal(signal.SIGINT, handle_interrupt)
 
 count = 0
 tot_size = 0
+all_stats = ("200", "301", "400", "401", "403", "404", "405", "500")
 my_dict = {}
 for line in sys.stdin:
     lst_line = line.split()
     count += 1
     tot_size += int(lst_line[8])
-    my_dict[lst_line[7]] = my_dict.get(lst_line[7], 0) + 1
+    if lst_line[7] in all_stats:
+        my_dict[lst_line[7]] = my_dict.get(lst_line[7], 0) + 1
     if count == 10:
         print(f"File size: {tot_size}")
         for key in sorted(my_dict.keys()):
