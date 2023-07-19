817fa77931279da60e0fb9316623f58aeca8a5d0
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-07-19 15:09:22 -0700

    Added a JOIN

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- This script uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tv_genres.name FROM tv_genres, tv_show_genres Where tv_show_genres.show_id = 8 AND tv_show_genres.genre_id = tv_genres.id ORDER BY tv_genres.name ASC
+SELECT tv_genres.name FROM tv_genres JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id Where tv_show_genres.show_id = 8 ORDER BY tv_genres.name ASC
