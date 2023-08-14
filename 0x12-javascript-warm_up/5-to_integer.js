4e30202b7926676db5bfca58c437a2f111ee70b8
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-14 22:58:37 +0000

    Added semi-colons

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const [,, arg] = process.argv;
 
 if (!isNaN(arg)) {
-  console.log('My number: ' + parseInt(arg))
+  console.log('My number: ' + parseInt(arg));
 } else {
-  console.log('Not a number')
-} 
+  console.log('Not a number');
+}
