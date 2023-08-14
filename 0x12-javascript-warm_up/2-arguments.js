3d88e8f90f389525eed26ef29bf786c314c15809
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-14 21:35:23 +0000

    debugging

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-let count = 0;
-
 if (argv.length > 2) {
-  count = 1;
-}
-
-if (count !== 0) {
   console.log('Argument found');
 } else {
   console.log('No argument');
