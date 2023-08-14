6a1bc73d249b94b37d882bcd0fedd9cd7f3d6af6
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-14 21:48:36 +0000

    Still searching for the problem

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,6 +3,8 @@ const argv = process.argv;
 
 if (argv.length - 2 === 0) {
   console.log('No argument');
+} else if (argv.length - 2 === 1) {
+  console.log('Argument found');
 } else {
   console.log('Argument found');
 }
