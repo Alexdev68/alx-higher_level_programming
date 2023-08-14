8abbf722ad8a77f2bc56028ee211a525dd709e6c
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-14 21:43:04 +0000

    Fixing unknown issue

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const argv = process.argv;
 
-if (argv.length > 2) {
-  console.log('Argument found');
-} else {
+if (argv.length - 2 === 0) {
   console.log('No argument');
+} else {
+  console.log('Argument found');
 }
