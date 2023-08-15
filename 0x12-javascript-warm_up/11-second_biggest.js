baf98b1d2e643e160ebdded71368c51a5bec2a5c
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-15 16:23:14 +0000

    Fixing bugs

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 const a = process.argv;
-const arr = [];
-let i;
 
-for (i = 2; i < a.length; i++) {
-  arr.push(parseInt(a[i]));
+function snd_largest(array) {
+  const arr = [];
+  let i;
+
+  for (i = 2; i < array.length; i++) {
+    arr.push(parseInt(a[i]));
+  }
+
+  if (isNaN(arr[0])) {
+    console.log(0);
+  } else if (arr.length === 1) {
+    console.log(0);
+  } else {
+    arr.sort();
+    let array1 = arr.reverse();
+    console.log(array1[1]);
+  }
 }
 
-if (isNaN(arr[0])) {
-  console.log(0);
-} else if (arr.length === 1) {
-  console.log(0);
-} else {
-  arr.sort();
-  let array = arr.reverse();
-  console.log(array[1]);
-}
+snd_largest(a);
