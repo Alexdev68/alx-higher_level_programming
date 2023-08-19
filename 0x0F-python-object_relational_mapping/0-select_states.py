ca5e53465674c20099a6fec5e74c5e9dfba01297
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-19 07:24:34 -0700

    Added more to the documentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module lists all states from the database hbtn_0e_0_usa"""
+"""This module lists all states from the database hbtn_0e_0_usa
+   jeuflwkjekwjnelwnejwk.jebkwhbrujwbkhwbkjel.wjnbjkgwbwkbrhjwbhwkb"""
 import MySQLdb
 from sys import argv
 
