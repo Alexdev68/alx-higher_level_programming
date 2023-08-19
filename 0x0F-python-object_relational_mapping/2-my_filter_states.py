8c50e6f937ee1bf4f7191c561fcbf995a1580cca
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-19 08:59:32 -0700

    New file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module displays all values in the states table of hbtn_0e_0_usa where
+   name matches the argument."""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
+    state = argv[4]
+
+    cur.execute(
+            "SELECT * FROM states WHERE name = %s ORDER BY states.id",
+            (state,)
+            )
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(f"{row}")
+
+    cur.close()
+    db.close()
