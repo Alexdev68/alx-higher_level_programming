3e3544866756b9e65eccca0e72249a4c0ba6626e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-19 08:01:01 -0700

    Now my code can not be executed when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""This module lists all states from the database hbtn_0e_0_usa
-   jeuflwkjekwjnelwnejwk.jebkwhbrujwbkhwbkjel.wjnbjkgwbwkbrhjwbhwkb"""
+"""This module lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-        )
-cur = db.cursor()
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+            )
+    cur = db.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY states.id")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
 
-rows = cur.fetchall()
+    rows = cur.fetchall()
 
-for row in rows:
-    print(f"{row}")
+    for row in rows:
+        print(f"{row}")
 
-cur.close()
-db.close()
+    cur.close()
+    db.close()
