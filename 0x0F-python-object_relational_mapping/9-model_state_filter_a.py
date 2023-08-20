43acb7aa662d6896897380a952e029e4ac0fa66e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-20 13:11:28 -0700

    New file

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module lists all states objects from the database hbtn_0e_6_usa"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
+        user, password, host, port, db_name))
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    rows = session.query(State).filter(State.name.like('%a%')).all()
+
+    for row in rows:
+        print("{}: {}".format(row.id, row.name))
+
+    session.close()
