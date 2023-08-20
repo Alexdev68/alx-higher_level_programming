08f4c509f7f7b3420e16293943de15b9ccd102aa
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-20 06:15:19 -0700

    New ile

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module contains the class definition of a State and an instance
+   Base = declarative_base()."""
+
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+user = "root"
+host = "localhost"
+port = 3306
+password = "root"
+db_name = "hbtn_0e_6_usa"
+
+
+class State(Base):
+    """This is the class that inherits from base."""
+
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+
+
+engine = create_engine(url="mysql+mysqldb://{}:{}@{}:{}/{}".format(
+    user, password, host, port, db_name))
+
+Base.metadata.create_all(engine)
