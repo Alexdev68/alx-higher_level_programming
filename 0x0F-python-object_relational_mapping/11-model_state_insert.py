86c0afbde8417ef4904fd83bae6ff07137243937
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-08-20 16:20:24 -0700

    New file

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(
+        argv[1], argv[2], "localhost", 3306, argv[3]))
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
+    session.commit()
+
+    rows = session.query(State).order_by(State.id).all()
+
+    for row in rows:
+        if row.name == 'Louisiana':
+            print(row.id)
+
+    session.close()
