04f1fff8a03ce8aa2efe274defbe1e7dede63d77
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-09-01 12:42:56 -0700

    Fixing

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-url=$1;response=$(curl -sl "$url");size=$(echo "$response" | awk '/Content-Lenght/ {print $2}');echo "$size"
+url=$1;response=$(curl -s -o /dev/null -w "%{size_download}" "$url");echo "$response"
