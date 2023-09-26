c6fd80c8815489571c258dfe27c25ffd000fbb77
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-09-26 11:10:41 -0700

    Fixed semistandard errors

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const process = require('process')
-const argv = process.argv
+const process = require('process');
+const argv = process.argv;
 
 fs.readFile(argv[2], 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-		return
-	}
-	console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 });
